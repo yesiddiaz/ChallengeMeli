949698037c6a5ddc25fcfa74e7f144462fa0711e
yediaz <yesid.diaz@mercadolibre.com.co>
2022-03-30 11:35:22 -0500

    Create DetailFragment and add dependencies from imageslider.

---
+++ app/src/main/java/com/meli/melichalllenge/ui/view/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.meli.melichalllenge.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.meli.melichalllenge.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+
+        return binding.root
+    }
+}
